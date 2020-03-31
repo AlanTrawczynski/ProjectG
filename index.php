bf2de0f5b2ec2a4139c8e2202c9dad1994f1d7f7
Alan <alantrawcz@gmail.com>
2020-03-31 23:19:49 +0200

    Esqueleto inicial

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <?php include 'headImports.php'?>
+    <Title>Timeline</Title>
+    
+</head>
+
+<body>
+
+    <?php include 'navbar.php'?>
+
+    <?php include 'bodyImports.php'?>
+
+</body>
+
+</html>
